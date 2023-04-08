36601e79fdbcffd5ab77b2e86b6e804c4c8bff3f
Florian Breit <breit.florian+github@icloud.com>
2023-04-08 16:22:25 +0200

    added first semi-nice looking product page

--- src/main/java/com/bfws121a/webshop/views/main/ProductView.java
+++ src/main/java/com/bfws121a/webshop/views/main/ProductView.java
@@ -1,12 +1,14 @@
 package com.bfws121a.webshop.views.main;
 
 import com.bfws121a.webshop.object.Product;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
-import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.BeforeEvent;
 import com.vaadin.flow.router.HasUrlParameter;
 import com.vaadin.flow.router.Route;
@@ -14,12 +16,12 @@ import com.vaadin.flow.router.Route;
 import java.awt.*;
 
 @Route(value = "product")
-public class ProductView extends Div implements HasUrlParameter<Integer> {
+public class ProductView extends HorizontalLayout implements HasUrlParameter<Integer> {
 
     Product product;
     Catalog catalog = new Catalog();
 
-    final private com.vaadin.flow.component.button.Button shoppingCart = new Button("In den Warenkorb", new Icon(VaadinIcon.CART));
+    final private Button shoppingCart = new Button("In den Warenkorb", new Icon(VaadinIcon.CART));
 
     @Override
     public void setParameter(BeforeEvent beforeEvent, Integer id) {
@@ -30,18 +32,46 @@ public class ProductView extends Div implements HasUrlParameter<Integer> {
             }
         }
 
-        if (product != null) {
-            Image productImage = new Image(product.getImage(), product.getName());
-            productImage.addClassName("image-prodPage");
-            Label productName = new Label(product.getName());
-            productName.addClassName("label-prodPage");
-            Label productPrice = new Label(String.valueOf(product.getPrice()));
-            productPrice.addClassName("label-prodPage");
-            shoppingCart.addClassName("cart");
-            add(productImage, productName, productPrice, shoppingCart);
-            this.setSizeFull();
-        } else {
-            setText("Could not find product");
-        }
+        // Product image
+        Image productImage = new Image(product.getImage(), product.getName());
+        productImage.addClassName("image-prodPage");
+
+        // Product name
+        Label productName = new Label(product.getName());
+        productName.addClassName("nameLabel-prodPage");
+
+        // Product price label
+        Label productPrice = new Label(String.valueOf(product.getPrice()) + " €");
+        productPrice.addClassName("priceLabel-prodPage");
+        shoppingCart.addClassName("cart-productPage");
+
+        // Divider
+        Label divider1 = new Label();
+        divider1.getElement().setProperty("innerHTML", "<hr />");
+        divider1.addClassName("divider1-prodPage");
+
+        // Description title label
+        Label descriptionTitle = new Label("Beschreibung");
+        descriptionTitle.addClassName("descriptionTitle-prodPage");
+
+        // Description label
+        Label productDescription = new Label();
+        productDescription.getElement().setProperty("innerHTML", product.getDescription());
+        productDescription.addClassName("description-prodPage");
+
+        // Divider
+        Label divider2 = new Label();
+        divider2.getElement().setProperty("innerHTML", "<hr />");
+        divider2.addClassName("divider2-prodPage");
+
+        // Reviews title label
+        Label reviewsTitle = new Label("Beschreibung");
+        reviewsTitle.addClassName("reviewsTitle-prodPage");
+
+        add(productImage, productName, productPrice, shoppingCart, divider1, descriptionTitle,
+                productDescription, divider2, reviewsTitle);
+
+        this.setClassName("grid-product");
+        this.setSizeFull();
     }
 }

db32e81c2295f27134655c98c823cafd175867a4
timheppi <timheppi@hotmail.de>
2023-04-15 13:16:19 +0200

    Dialog der bei Kaufabwicklung auf die Kontaktseite verweist

--- src/main/java/com/bfws121a/webshop/views/main/ProductTile.java
+++ src/main/java/com/bfws121a/webshop/views/main/ProductTile.java
@@ -1,6 +1,7 @@
 package com.bfws121a.webshop.views.main;
 
 import com.bfws121a.webshop.object.Product;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
@@ -12,18 +13,28 @@ public class ProductTile extends VerticalLayout {
 
     private Image productImage;
     private Label productName;
+    private Label productID;
     private Label productPrice;
-    final private Button shoppingCart = new Button("In den Warenkorb", new Icon(VaadinIcon.CART));
+
+    final private Button shoppingCart = new Button("Kaufabwicklung", new Icon(VaadinIcon.CART));
 
     public ProductTile(Product pro) {
         productImage = new Image(pro.getImage(), pro.getName());
         productImage.addClassName("image-prodTile");
         productName = new Label(pro.getName());
         productName.addClassName("label-prodTile");
+        productID = new Label("Artikelnummer: " +pro.getId());
+        productID.addClassName("idLabel-prodPage");
+
         productPrice = new Label(pro.getPrice() + "€");
         productPrice.addClassName("label-prodTile");
         shoppingCart.addClassName("cart");
-        add(productImage, productName, productPrice, shoppingCart);
+        shoppingCart.addClickListener(e -> {
+            BuyDialog dialog = new BuyDialog(pro);
+            dialog.open();
+        });
+
+        add(productImage, productName, productID, productPrice, shoppingCart);
         this.getStyle().set("margin-right", "10px");
 
         // navigate to selected product

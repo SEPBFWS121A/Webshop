aa9ddf59199c86d6be2c1d23795b8402903b3788
Florian Breit <breit.florian@icloud.com>
2023-05-17 19:04:46 +0200

    add productive database add integration test for searching by product name (productive db) add integration test for finding all products (productive db)

---
+++ src/test/java/com/bfws121a/webshop/repositories/H2ProdRepoFindAllTestProductive.java
@@ -0,0 +1,32 @@
+package com.bfws121a.webshop.repositories;
+
+import com.bfws121a.webshop.object.Product;
+import com.bfws121a.webshop.services.ProductService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class H2ProdRepoFindAllTestProductive {
+
+    private H2ProductRepository repository;
+
+    @Test
+    public void findAllTest () {
+        repository = new H2ProductRepository("jdbc:h2:./webshop", "sa", "");
+
+        ProductService productService;
+        productService = new ProductService(repository);
+        List<Product> productList = productService.findAll();
+
+        System.out.println("\n");
+        for (Product product : productList) {
+            System.out.println(product.toString() + "\n");
+        }
+
+        Assertions.assertEquals(9, productList.size());
+    }
+
+}

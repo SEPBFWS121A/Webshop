22e15c71d6f3f4ed70bf1e91610f79316c26d60c
Florian Breit <77027788+flnbrt@users.noreply.github.com>
2023-05-23 09:41:28 +0200

    fix test-db create statement

--- src/test/java/com/bfws121a/webshop/repositories/H2ProdRepoSearchByNameTest.java
+++ src/test/java/com/bfws121a/webshop/repositories/H2ProdRepoSearchByNameTest.java
@@ -26,7 +26,8 @@ public class H2ProdRepoSearchByNameTest {
                     "    Description character varying,\n" +
                     "    Type varchar(256),\n" +
                     "    Theme varchar(256),\n" +
-                    "    Price int\n" +
+                    "    Price int,\n" +
+                    "    producttags varchar(255)\n" +
                     ");");
             //floriansMom.executeQuery("select * from COLUMNS");
             floriansMom.execute("INSERT INTO product(Image, Name, Description, Type, Theme, Price, producttags) VALUES ('icons/Hogwards.png', 'Hogwards', 'Erlebe die Magie von Hogwards mit diesem atemberaubenden Baustein Set! Das Set umfasst die berühmte Schule für Hexerei und Zauberei in all ihrer majestätischen Pracht.<p>\n\n<p>Das Set enthält die verschiedenen Türme und Gebäude von Hogwarts, einschließlich des Uhrturms, der Eulerei und des Gryffindor-Turms. Jeder Turm ist detailreich gestaltet und bietet zahlreiche versteckte Überraschungen und Geheimnisse. <p>\\n\n <p>Das Set enthält auch eine Vielzahl von Figuren aus der Harry-Potter-Saga, darunter Harry Potter, Hermine Granger, Ron Weasley, Professor Dumbledore, Professor Snape, Hagrid und viele mehr. Jede Figur ist mit detaillierten Accessoires und Kostümen ausgestattet und bietet unzählige Möglichkeiten zum Spielen und Erkunden.', 'Set', 'Harry Potter',49999, 'bestseller')");
@@ -46,7 +47,7 @@ public class H2ProdRepoSearchByNameTest {
     }
 
     @Test
-    public void searchRepoTest () {
+    public void searchRepoTest() {
         repository = new H2ProductRepository("jdbc:h2:./test", "sa", "");
         ProductService productService;
         productService = new ProductService(repository);

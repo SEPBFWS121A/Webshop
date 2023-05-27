d3b1def672d8acdfdf3a91b836c47f39f32d9fec
timheppi <timheppi@hotmail.de>
2023-05-27 15:15:40 +0200

    Update-Suitcase und POm

--- src/test/java/com/bfws121a/webshop/JUnitTestSuite.java
+++ src/test/java/com/bfws121a/webshop/JUnitTestSuite.java
@@ -1,14 +1,10 @@
 package com.bfws121a.webshop;
 
 import com.bfws121a.webshop.repositories.*;
-import org.junit.platform.suite.api.SelectPackages;
-import org.junit.platform.suite.api.Suite;
 import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-
-@Suite
-@SelectPackages("com/bfws121a/webshop/repositories")
-/*@RunWith(Suite.class)
+@RunWith(Suite.class)
 
 @Suite.SuiteClasses({
         H2ProdDeleteByNameTest.class,
@@ -18,7 +14,7 @@ import org.junit.runner.RunWith;
         H2ProdRepoSearchByNameTest.class,
         H2ReviewRepoAddReviewTest.class,
         H2ReviewRepoFindByProdIDTest.class
-})*/
+})
 
 public class JUnitTestSuite {
 }

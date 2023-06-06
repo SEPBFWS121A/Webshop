ba4b0f56efb26a0ad7841dd902d9bfffc576929a
Florian Breit <77027788+flnbrt@users.noreply.github.com>
2023-06-06 09:14:59 +0200

    fix VATCalculatorTest

--- src/test/java/com/bfws121a/webshop/unitTest/VATCalculatorTest.java
+++ src/test/java/com/bfws121a/webshop/unitTest/VATCalculatorTest.java
@@ -3,24 +3,25 @@ package com.bfws121a.webshop.unitTest;
 import com.bfws121a.webshop.helper.Calculator;
 import com.bfws121a.webshop.object.Cart;
 import com.bfws121a.webshop.object.Product;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.ArrayList;
 import java.util.List;
+
+@SpringBootTest
 public class VATCalculatorTest {
 
-    private Product prod1;
-    private Product prod2;
-    private List<Cart> shoppingCart = new ArrayList<>();
+    private static Product prod1;
+    private static Product prod2;
+    private static List<Cart> shoppingCart = new ArrayList<>();
 
-    private Calculator calculator;
+    private static Calculator calculator;
 
-    @Before
-    public void setupCart() {
+    @BeforeAll
+    public static void setupCart() {
         prod1 = new Product(1, "icons/Hogwards.png", "Hogwards", "test", "Set", "Harry Potter",49999, "bestseller");
         prod2 = new Product(2, "icons/Millenium Falke.webp", "Millenium Falke", "test", "Set", "Star Wars", 99999, "neu");
         shoppingCart.add(new Cart(prod1));
@@ -31,7 +32,7 @@ public class VATCalculatorTest {
     @Test
     public void calculateVAT() {
         int value = (int) calculator.calculateVAT();
-        Assert.assertEquals(24029, value);
+        Assertions.assertEquals(24029, value);
     }
 
 

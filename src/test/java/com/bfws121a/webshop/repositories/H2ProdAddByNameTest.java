e5466398a32fb20f2d29a7adcdb92b638325e29b
RobinsKonto8 <130449780+RobinsKonto8@users.noreply.github.com>
2023-06-18 12:06:46 +0200

    Update H2ProdAddByNameTest.java

--- src/test/java/com/bfws121a/webshop/repositories/H2ProdAddByNameTest.java
+++ src/test/java/com/bfws121a/webshop/repositories/H2ProdAddByNameTest.java
@@ -14,8 +14,9 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.List;
 
+//BY Robin
 @SpringBootTest
-public class H2ProdDeleteByNameTest {
+public class H2ProdAddByNameTest {
 
     private H2ProductRepository repository;
 
@@ -33,8 +34,6 @@ public class H2ProdDeleteByNameTest {
                     "    Price int,\n" +
                     "    producttags varchar(255)\n" +
                     ");");
-            stmt.execute("INSERT INTO product(Image, Name, Description, Type, Theme, Price, producttags) VALUES ('icons/Hogwards.png', 'Hogwards', 'Erlebe die Magie von Hogwards mit diesem atemberaubenden Baustein Set! Das Set umfasst die berühmte Schule für Hexerei und Zauberei in all ihrer majestätischen Pracht.<p>\n\n<p>Das Set enthält die verschiedenen Türme und Gebäude von Hogwarts, einschließlich des Uhrturms, der Eulerei und des Gryffindor-Turms. Jeder Turm ist detailreich gestaltet und bietet zahlreiche versteckte Überraschungen und Geheimnisse. <p>\\n\n <p>Das Set enthält auch eine Vielzahl von Figuren aus der Harry-Potter-Saga, darunter Harry Potter, Hermine Granger, Ron Weasley, Professor Dumbledore, Professor Snape, Hagrid und viele mehr. Jede Figur ist mit detaillierten Accessoires und Kostümen ausgestattet und bietet unzählige Möglichkeiten zum Spielen und Erkunden.', 'Set', 'Harry Potter',49999, 'bestseller')");
-            stmt.execute("INSERT INTO product(Image, Name, Description, Type, Theme, Price, producttags) VALUES ('icons/Millenium Falke.webp', 'Millenium Falke','Willkommen an Bord des größten und detailreichsten Modells des Star Wars Millennium Falcon, das jemals geschaffen wurde. Mit seinen 7.500 Teilen ist es eines der größten Baustein Modelle überhaupt! Diese fantastische Baustein Interpretation von Han Solos unvergesslichem Corellian-Frachter besitzt sämtliche Details, die sich Star Wars Fans jeden Alters nur wünschen können. Hierzu zählen auch die aufwendigen Details an der Außenseite, die Vierlingslaserkanonen oben und unten, die Landebeine, die absenkbare Einstiegsrampe und das Cockpit mit abnehmbarem Kabinendach für 4 Minifiguren. Nimm die einzelnen Rumpfplatten ab, um den äußerst detailreichen Innenbereich aus Hauptfrachtraum, Heckfrachtraum und Geschützstation zum Vorschein zu holen. Darüber hinaus verfügt dieses faszinierende Modell über austauschbare Sensorschüsseln und eine austauschbare Crew, damit du entscheiden kannst, ob du lieber klassische Star Wars Abenteuer mit Han, Leia, Chewbacca und C-3PO nachspielst oder dich mit dem älteren Han sowie mit Rey, Finn und BB-8 in die Welt aus Episode VII und VIII begibst!', 'Set', 'Star Wars', 99999, 'new')");
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -51,12 +50,24 @@ public class H2ProdDeleteByNameTest {
     }
 
     @Test
-    public void deleteByNameTest() {
+    public void addByNameTest() {
         repository = new H2ProductRepository("jdbc:h2:./test", "sa", "");
         ProductService productService = new ProductService(repository);
-        productService.deleteByName("Hogwards");
+        
+        Product newProduct = new Product();
+        newProduct.setImage("icons/NewProduct.png");
+        newProduct.setName("New Product");
+        newProduct.setDescription("This is a new product");
+        newProduct.setType("Type");
+        newProduct.setTheme("Theme");
+        newProduct.setPrice(999);
+        newProduct.setProductTags("new, featured");
+        
+        productService.addProduct(newProduct);
+        
         List<Product> productList = productService.findAll();
 
         Assertions.assertEquals(1, productList.size());
+        Assertions.assertEquals("New Product", productList.get(0).getName());
     }
 }
